003d28a584a5d878d97c85e7efaccb54c7b59c0f
patrickse <patrick@sernetz.com>
2026-03-04 14:59:54 +0100

    initial

---
+++ services/ble-scale-sync.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    ;
+
+  cfg = config.services.ble-scale-sync;
+
+in
+{
+
+  options.services.ble-scale-sync =
+    let
+      inherit (lib) types;
+    in
+    {
+      enable = mkEnableOption "BLE-SCALE-Sync Daemon";
+
+      home = mkOption {
+        type = types.str;
+        default = "/var/lib/ble-scale-sync";
+        example = "/var/lib/ble-scale-sync";
+        description = "Home directory for ble-scale-sync";
+      };
+
+      config = mkIf cfg.enable {
+
+      };
+
+    };
+
+}
